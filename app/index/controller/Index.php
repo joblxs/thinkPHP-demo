842f77b04abee1aa7595c459b534949ef48ba664
shuai <1591451405@qq.com>
2024-05-25 10:45:59 +0800

    添加主页

--- app/index/controller/Index.php
+++ app/index/controller/Index.php
@@ -8,6 +8,11 @@ class Index extends BaseController
 {
     public function index()
     {
+        return view('index');
+    }
+
+    public function index1()
+    {
         $html = "<div style='width:100%;height:100%;font-size: 20px;text-align: center;'>
             欢迎来到api管理系统，<a href='/apidoc'>点击跳转</a>
         </div>";
