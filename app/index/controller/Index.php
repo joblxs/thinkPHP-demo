5f506c76466c07825342b601f5b4beee811ba5dc
shuai <1591451405@qq.com>
2024-05-25 10:54:14 +0800

    删除无用信息

--- app/index/controller/Index.php
+++ app/index/controller/Index.php
@@ -10,12 +10,4 @@ class Index extends BaseController
     {
         return view('index');
     }
-
-    public function index1()
-    {
-        $html = "<div style='width:100%;height:100%;font-size: 20px;text-align: center;'>
-            欢迎来到api管理系统，<a href='/apidoc'>点击跳转</a>
-        </div>";
-        return $html;
-    }
 }
