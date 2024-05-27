d4cc812e634bb56e22b6b830da9ecb35efe050e9
lixiaoshuai <xiaoshuai.li@istarshine.com>
2024-05-27 14:10:57 +0800

    随机图片

--- app/api/controller/Lsky.php
+++ app/api/controller/Lsky.php
@@ -116,11 +116,85 @@ class Lsky extends BaseController
         Apidoc\Url("/api/lsky/randomImages"),
         Apidoc\Header(name:"Content-Type",type: "string",require: true,desc: "Content-Type 编码请求",mock: "application/json"),
         Apidoc\Query(name:"category",type: "string",require: true,desc: "类别：风景、汽车、星空、手机壁纸、24节气、动漫"),
+        Apidoc\Returned("data",type: "array",desc: "data"),
+    ]
+    public function randomImages(Request $request) {
+        $category = $request->get('category', '');
+        $album = [
+            ["id" => 8, "name" => "风景"],
+            ["id" => 7, "name" => "汽车"],
+            ["id" => 6, "name" => "星空"],
+            ["id" => 5, "name" => "手机壁纸"],
+            ["id" => 4, "name" => "24节气"],
+            ["id" => 3, "name" => "动漫"]
+        ];
+        $album_id = 0;
+        if (!empty($category)) {
+            foreach ($album as $item) {
+                if ($item["name"] == $category) {
+                    $album_id = $item["id"];
+                }
+            }
+        }
+
+        if ($album_id == 0) {
+            // 使用array_rand随机获取一个数组的键
+            $random_key = array_rand($album);
+            // 使用随机键获取对应的id
+            $album_id = $album[$random_key]['id'];
+        }
+        try {
+            $response = Http::asJson()->withHeaders([
+                'Authorization' => self::$token
+            ])->get(self::$apiUrl . "/images", [
+                'page' => 1, 'order' => 'newest', 'album_id' => $album_id
+            ]);
+
+            $imageArr = [];
+            if ($response->status() == 200) {
+                $data = $response->json()->data->data;
+                foreach ($data as $key => $value) {
+                    array_push($imageArr, $value->links->url);
+                }
+                $randomImage = '';
+                if (count($imageArr) > 0) {
+                    $randomImage = $imageArr[array_rand($imageArr)];
+                }
+                return redirect($randomImage);
+//                ob_start(); // 开始输出缓冲
+//
+//                $response = Http::asJson()->get($randomImage);
+//                $image_data = $response->body();
+//                $image = imagecreatefromstring($image_data);
+//                // 输出调整尺寸后的图片
+//                header('Content-Type: image/webp');
+//                imagewebp($image);
+//                // 释放资源
+//                imagedestroy($image);
+//
+//                $output = ob_get_clean(); // 获取输出缓冲并清空
+//                echo $output; // 输出缓冲内容（如果有的话）
+//                return redirect($randomImage);
+            }
+            return json($response->status());
+        } catch (ErrorException $e) {
+            var_dump($e);
+            return $e;
+        }
+    }
+
+    #[
+        Apidoc\Title("返回一张随机图片（可指定尺寸）"),
+        Apidoc\Tag("兰空"),
+        Apidoc\Method("GET"),
+        Apidoc\Url("/api/lsky/randomSizeImages"),
+        Apidoc\Header(name:"Content-Type",type: "string",require: true,desc: "Content-Type 编码请求",mock: "application/json"),
+        Apidoc\Query(name:"category",type: "string",require: true,desc: "类别：风景、汽车、星空、手机壁纸、24节气、动漫"),
         Apidoc\Query(name:"width",type: "int",require: true,desc: "图片宽度（默认300）"),
         Apidoc\Query(name:"height",type: "int",require: false,desc: "图片高度（不传自行计算宽高比）"),
         Apidoc\Returned("data",type: "array",desc: "data"),
     ]
-    public function randomImages(Request $request) {
+    public function randomSizeImages(Request $request) {
         $category = $request->get('category', '');
         $width = $request->get('width', 300);
         $height = $request->get('height', 0);
