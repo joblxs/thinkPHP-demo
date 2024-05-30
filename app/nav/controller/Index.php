34d975f6cc99323c9cf81308805066e5059564d9
lixiaoshuai <xiaoshuai.li@istarshine.com>
2024-05-30 14:08:46 +0800

    重定义结构

--- app/nav/controller/Index.php
+++ app/nav/controller/Index.php
@@ -8,92 +8,6 @@ class Index extends BaseController
 {
     public function index()
     {
-        $arr = [
-            [
-                'id' => '1',
-                'pid' => '0',
-                'name' => '个人站点',
-                'children' => [
-                    [
-                        'id' => '1',
-                        'pid' => '1',
-                        'name' => '拾贝',
-                        'desc' => '拾贝，一个专注于个人技术分享的博客。',
-                        'link' => 'https://lxshuai.top/'
-                    ],
-                    [
-                        'id' => '2',
-                        'pid' => '1',
-                        'name' => '拾贝2',
-                        'desc' => '拾贝，一个专注于个人技术分享的博客。',
-                        'link' => 'https://lxshuai.top/'
-                    ]
-                ]
-            ],
-            [
-                'id' => '2',
-                'pid' => '0',
-                'name' => '工具箱',
-                'children' => [
-                    [
-                        'id' => '3',
-                        'pid' => '2',
-                        'name' => '常用工具',
-                        'children' => [
-                            [
-                                'id' => '3',
-                                'pid' => '3',
-                                'name' => '拾贝3',
-                                'desc' => '拾贝，一个专注于个人技术分享的博客。',
-                                'link' => 'https://lxshuai.top/'
-                            ],
-                            [
-                                'id' => '4',
-                                'pid' => '3',
-                                'name' => '拾贝4',
-                                'desc' => '拾贝，一个专注于个人技术分享的博客。',
-                                'link' => 'https://lxshuai.top/'
-                            ]
-                        ]
-                    ],
-                    [
-                        'id' => '4',
-                        'pid' => '2',
-                        'name' => '常用工具2',
-                        'children' => [
-                            [
-                                'id' => '5',
-                                'pid' => '4',
-                                'name' => '拾贝5',
-                                'desc' => '拾贝，一个专注于个人技术分享的博客。',
-                                'link' => 'https://lxshuai.top/'
-                            ],
-                            [
-                                'id' => '6',
-                                'pid' => '4',
-                                'name' => '拾贝6',
-                                'desc' => '拾贝，一个专注于个人技术分享的博客。',
-                                'link' => 'https://lxshuai.top/'
-                            ]
-                        ]
-                    ]
-                ]
-            ]
-        ];
-        foreach ($arr as $key => $val) {
-            $child = array_column($val['children'], 'children');
-            if (count($child) > 0) {
-                $val['type'] = 'categories';
-            } else {
-                $val['type'] = 'link';
-            }
-            $arr[$key] = $val;
-        }
-//        return json($arr);
-        return view('index', ['nav' => $arr]);
-    }
-
-    public function demo() {
         $cat = [
             [
                 'id' => 1,
@@ -162,7 +76,7 @@ class Index extends BaseController
                 ]
             ],
         ];
-        return view('demo', [
+        return view('index', [
             'cat' => $cat,
             'link' => $link
         ]);
