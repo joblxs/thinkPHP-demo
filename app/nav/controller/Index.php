66b800f305e15fb83dfda80667859b3e417b8321
shuai <1591451405@qq.com>
2024-06-01 02:16:53 +0800

    修改导航页

--- app/nav/controller/Index.php
+++ app/nav/controller/Index.php
@@ -11,12 +11,30 @@ class Index extends BaseController
 {
     public function index()
     {
-        $cat = Categories::getAllCategoriesTree();
-        // 将树状结构转换为JSON格式输出
+        $cat = Categories::getMenuList(1);
         $link = Links::getAllLinks();
         return view('index', [
             'cat' => $cat,
             'link' => $link
         ]);
+    }
+
+    public function api() {
+        $homeInfo = [
+            'title' => '首页',
+            'href'  => 'page/welcome-1.html?t=1',
+        ];
+        $logoInfo = [
+            'title' => '个人导航',
+            'image' => 'https://pic.lxshuai.top/i/2024/05/18/6647eeade3ec2.webp',
+            'href' => '/'
+        ];
+        $menuInfo = Categories::getMenuList();
+        $systemInit = [
+            'homeInfo' => $homeInfo,
+            'logoInfo' => $logoInfo,
+            'menuInfo' => $menuInfo,
+        ];
+        return json($systemInit);
     }
 }
--- app/nav/model/Categories.php
+++ app/nav/model/Categories.php
@@ -1,43 +1,42 @@
 <?php
 namespace app\nav\model;
 
+use think\facade\Db;
 use think\Model;
 
 class Categories extends Model
 {
     // 设置当前模型对应的完整数据表名称
-    protected $table = 'nav_categories';
+    protected $table = 'nav_categorie';
     // 设置时间戳字段名
-    protected $createTime = 'created_at';
-    protected $updateTime = 'updated_at';
+    protected $createTime = 'create_at';
+    protected $updateTime = 'update_at';
+    protected $deleteTime = 'delete_at';
 
-    // 获取所有分类并构建树状结构
-    public static function getAllCategoriesTree()
-    {
-        $categories = self::field(['id', 'pid', 'name', 'icon'])
-            ->where('is_deleted', 0)
-            ->where('sort', 0)
-            ->select()->toArray();
-        return self::buildTree($categories);
+    // 获取菜单列表
+    public static function getMenuList($pid = 0){
+        $menuList = self::field('id,pid,title,icon,href,target')
+            ->where('status', 1)
+            ->order('sort', 'desc')
+            ->select();
+        $menuList = self::buildMenuChild($pid, $menuList);
+        return $menuList;
     }
 
-    // 递归构建树状结构
-    public static function buildTree($data, $parentId = 0, $level = 0)
-    {
-        $tree = [];
-        foreach ($data as $item) {
-            if ($item['pid'] == $parentId) {
-                $item['level'] = $level; // 添加层级信息
-                $item['children'] = self::buildTree($data, $item['id'], $level + 1);
-                $tree[] = $item;
+    //递归获取子菜单
+    public static function buildMenuChild($pid, $menuList){
+        $treeList = [];
+        foreach ($menuList as $v) {
+            if ($pid == $v['pid']) {
+                $node = $v;
+                $child = self::buildMenuChild($v['id'], $menuList);
+                if (!empty($child)) {
+                    $node['child'] = $child;
+                }
+                // todo 后续此处加上用户的权限判断
+                $treeList[] = $node;
             }
         }
-        return $tree;
-    }
-
-    // 定义子分类的关联
-    public function children()
-    {
-        return $this->hasMany(self::class, 'pid', 'id');
+        return $treeList;
     }
 }
